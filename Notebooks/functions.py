84f79d04d1148853892b5393b3c4b6cc8d8f14b4
Kevin Nourian <knourian@outlook.com>
2024-07-31 12:55:44 +0300

    EDA Continues

--- Notebooks/functions.py
+++ Notebooks/functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+# Missing Values
 def MissingValues(data):
     
     name_cols=data.columns[data.isna().any()].tolist()
@@ -10,6 +11,7 @@ def MissingValues(data):
     return missing_values
 
 
+# Unique Values
 def UniqueValues(data):
     
     categorical_columns_list = data.select_dtypes(include=['object']).columns.tolist()
@@ -22,11 +24,13 @@ def UniqueValues(data):
         print(f'Unique Values in {column}: {values}')
 
 
+# Duplicates
 def Duplicates(data):
 
     print(f'Duplicates: {data.duplicated().sum()}, ({np.round(100*data.duplicated().sum()/len(data),1)}%)')
 
 
+# Outliers
 def Outliers(data):
     
     numeric_data = data.select_dtypes(include=['number'])
@@ -40,3 +44,40 @@ def Outliers(data):
 
     outlier_counts = outliers.sum()
     print (outlier_counts)
+
+
+# Side-by-Side Bar Plots
+def side_by_side_barplot(data_1, data_2, title_1, title_2, labels, feature, y, palette):
+
+    '''
+    Creates a side-by-side bar plot comparing two datasets.
+    '''
+
+    plt.rcParams.update(params)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 8))
+
+    sns.barplot(data=data_1, x=feature, y=y, ax=ax1, palette=palette)
+    sns.barplot(data=data_2, x=feature, y=y, ax=ax2, palette=palette)
+
+    ax1.set_xlabel(feature)
+    ax1.set_ylabel(y)
+    ax2.set_xlabel(feature)
+    ax2.set_ylabel(y)
+
+    total_count1 = data_1[y].sum()
+    for container in ax1.containers:
+        labels = [f'{(v.get_height() / total_count1 * 100):.1f}%' for v in container]
+        ax1.bar_label(container, labels=labels, size=size)
+
+    total_count2 = data_2[y].sum()
+    for container in ax2.containers:
+        labels = [f'{(v.get_height() / total_count2 * 100):.1f}%' for v in container]
+        ax2.bar_label(container, labels=labels, size=size)
+
+    ax1.set_title(title_1)
+    ax2.set_title(title_2)
+
+    sns.despine()
+
+    plt.show()    
